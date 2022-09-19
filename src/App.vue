cfb399bbd091c5b4f186ff95dfa1e6fa2fb88ca3
eugreg <daiaegregory@gmail.com>
2022-09-19 09:25:04 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cep: "",
+    };
+  },
+  methods: {
+    async search() {
+      const url = `https://www.cepaberto.com/api/v3/cep?cep=${this.cep}`;
+      this.resultado = await axios.get(url, {
+        headers: {
+          Authorization: "token token=0991bb7dd912811e5252a9b907d19311",
+        },
+      });
+    },
+  },
+};
+</script>
+<template>
+  <main>
+    <h1>Busca Cep</h1>
+    <form @submit.prevent="search">
+      <input type="text" v-model="cep" />
+      <button type="submit">buscar</button>
+    </form>
+    {{ resultado }}
+  </main>
+</template>
